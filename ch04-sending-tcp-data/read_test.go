82fd8b1b61a6e3bd089d9ef7eee7ab70df2905ab
Mohammadreza Hosseini <moha.rezahosseini@gmail.com>
2026-02-11 21:47:15 +0100

    Add explanation to read_test

--- ch04-sending-tcp-data/read_test.go
+++ ch04-sending-tcp-data/read_test.go
@@ -8,7 +8,15 @@ import (
 )
 
 // ## Reading Data into a Fixed Buffer
-
+// TCP connections in Go implement the `io.Reader` interface, which allows you to read data from the network connection.
+// 	- To read data from a network connection, you need to provide a buffer for the network connection’s Read method to fill.
+// 	- The Read method will populate the buffer to its capacity if there is enough data in the connection’s receive buffer.
+//		- If there are fewer bytes in the receive buffer than the capacity of the buffer you provide,
+//		- Read will populate the given buffer with the data and return instead of waiting for more data to arrive.
+//		- In other words, Read is not guaranteed to fill your buffer to capacity before it returns.
+//	- Listing 4-1 demonstrates the process of reading data from a network connection into a byte slice.
+//
+// Listing 4-1: Receiving data over a network connection
 func TestReadIntoBuffer(t *testing.T) {
 	payload := make([]byte, 1<<24) // 16 MB
 	_, err := rand.Read(payload)   // generate a random payload
