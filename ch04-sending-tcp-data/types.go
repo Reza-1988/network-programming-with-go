b290ad8be88f790fafbc29c881f4fa9412dd352e
Mohammadreza Hosseini <moha.rezahosseini@gmail.com>
2026-02-17 21:28:30 +0100

    Add List 4-6 from types.go

--- ch04-sending-tcp-data/types.go
+++ ch04-sending-tcp-data/types.go
@@ -77,25 +77,54 @@ type Payload interface { //(4)
 }
 
 // Listing 4-5: Creating the Binary type
-type Binary []byte
+type Binary []byte // (1)
 
-func (m Binary) Bytes() []byte  { return m }
-func (m Binary) String() string { return string(m) }
+func (m Binary) Bytes() []byte  { return m }         // (2)
+func (m Binary) String() string { return string(m) } // (3)
 
-func (m Binary) WriteTo(w io.Writer) (int64, error) {
-	err := binary.Write(w, binary.BigEndian, BinaryType) // 1-byte type
+func (m Binary) WriteTo(w io.Writer) (int64, error) { // (4)
+	err := binary.Write(w, binary.BigEndian, BinaryType) // 1-byte type (5)
 	if err != nil {
 		return 0, err
 	}
 	var n int64 = 1
 
-	err = binary.Write(w, binary.BigEndian, uint32(len(m))) // 4-byte size
+	err = binary.Write(w, binary.BigEndian, uint32(len(m))) // 4-byte size (6)
 	if err != nil {
 		return n, err
 	}
 	n += 4
 
-	o, err := w.Write(m) // payload
+	o, err := w.Write(m) // payload (7)
+
+	return n + int64(o), err
+}
+
+// Listing 4-6: Completing the Binary type’s implementation ( types go)
+
+func (m *Binary) ReadFrom(r io.Reader) (int64, error) {
+	var typ uint8
+	err := binary.Read(r, binary.BigEndian, &typ) // 1-byte type
+	if err != nil {
+		return 0, err
+	}
+	var n int64 = 1
+	if typ != BinaryType {
+		return n, errors.New("invalid Binary")
+	}
+
+	var size uint32
+	err = binary.Read(r, binary.BigEndian, &size) // 4-byte size
+	if err != nil {
+		return n, err
+	}
+	n += 4
+	if size > MaxPayloadSize {
+		return n, ErrMaxPayloadSize
+	}
+
+	*m = make([]byte, size)
+	o, err := r.Read(*m) // payload
 
 	return n + int64(o), err
 }
