877ef46fef5c3607ab537722c1e337f94de9f56a
Mohammadreza Hosseini <moha.rezahosseini@gmail.com>
2026-03-09 18:53:11 +0100

    added Listing 4-16 to proxy_test.go

--- ch04-sending-tcp-data/proxy_test.go
+++ ch04-sending-tcp-data/proxy_test.go
@@ -1,6 +1,11 @@
 package ch04
 
-import "io"
+import (
+	"io"
+	"net"
+	"sync"
+	"testing"
+)
 
 // Listing 4-15: Proxy data between a reader and writer
 // 	- This proxy function (1) is a bit more useful in that it accepts the ubiquitous `io.Reader` and `io.Writer` interfaces instead of `net.Conn`.
@@ -19,3 +24,58 @@ func proxy(from io.Reader, to io.Writer) error { // (1)
 	_, err := io.Copy(to, from)
 	return err
 }
+
+// Listing 4-16: Creating the listener
+
+func TestProxy(t *testing.T) {
+	var wg sync.WaitGroup
+
+	// server listens for a "ping" message and responds with a
+	// "pong" message. All other messages are echoed back to the client.
+	server, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				return
+			}
+
+			go func(c net.Conn) {
+				defer c.Close()
+
+				for {
+					buf := make([]byte, 1024)
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							t.Error(err)
+						}
+
+						return
+					}
+
+					switch msg := string(buf[:n]); msg {
+					case "ping":
+						_, err = c.Write([]byte("pong"))
+					default:
+						_, err = c.Write(buf[:n])
+					}
+
+					if err != nil {
+						if err != io.EOF {
+							t.Error(err)
+						}
+
+						return
+					}
+				}
+			}(conn)
+		}
+	}()
